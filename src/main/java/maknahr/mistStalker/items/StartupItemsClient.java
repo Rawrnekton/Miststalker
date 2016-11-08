5ba63c8d77aa2f31f3a75b2029dd8a1d6a3e5384
maknahr <jonathangerdes1@aol.de>
2016-11-08 14:31:06 +0100

    started adding the first item, has to be added to startup though (and completely rewritten)

---
+++ src/main/java/maknahr/mistStalker/items/StartupItemsClient.java
@@ -0,0 +1,23 @@
+package maknahr.mistStalker.items;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+
+public class StartupItemsClient {
+
+  public static void preInitClientOnly()
+  {
+    // required in order for the renderer to know how to render your item.
+    ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation("mistStalker:itemWayfarersStone", "inventory");
+    final int DEFAULT_ITEM_SUBTYPE = 0;
+    ModelLoader.setCustomModelResourceLocation(StartupItemsCommon.itemWayfarersStone, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+  }
+
+  public static void initClientOnly()
+  {
+  }
+
+  public static void postInitClientOnly()
+  {
+  }
+}
---
+++ src/main/java/maknahr/mistStalker/items/itemWayfarersStone.java
@@ -0,0 +1,33 @@
+package maknahr.mistStalker.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+/**
+ * User: The Grey Ghost
+ * Date: 30/12/2014
+ *
+ * ItemSimple is an ordinary two-dimensional item
+ * For background information on items see here http://greyminecraftcoder.blogspot.com/2013/12/items.html
+ *   and here http://greyminecraftcoder.blogspot.com.au/2014/12/item-rendering-18.html
+ */
+
+public class itemWayfarersStone extends Item{
+
+	  public itemWayfarersStone() {
+		
+	    final int MAXIMUM_NUMBER_OF_FROGS = 6;
+	    this.setMaxStackSize(MAXIMUM_NUMBER_OF_FROGS);
+	    this.setCreativeTab(CreativeTabs.MISC);   // the item will appear on the Miscellaneous tab in creative
+	  }
+}
+/*
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+
+public class ItemSimple extends Item
+{
+
+
+}*/
