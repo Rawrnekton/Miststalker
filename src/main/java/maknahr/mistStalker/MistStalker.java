4ea4ec0882737242730194408286498ae58b8982
Rawrnekton <jonathangerdes1@aol.de>
2016-11-29 09:46:39 +0100

    what a commit

--- src/main/java/maknahr/mistStalker/MistStalker.java
+++ src/main/java/maknahr/mistStalker/MistStalker.java
@@ -1,7 +1,6 @@
 package maknahr.mistStalker;
 
 import maknahr.mistStalker.proxy.CommonProxy;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -15,6 +14,9 @@ public class MistStalker {
     public static final String VERSION = "0.1";
 	public static final String NAME = "Miststalker";
     
+	
+	/**
+	 * if i ever need this it'll be here BUT i will remove it once i have a playable version
 	//just put that here just in case i need it later and forgot where to put it
     static {
         //For some reason, this has to be done here
@@ -23,7 +25,7 @@ public class MistStalker {
     
 	@Mod.Instance(MODID)
 	public static MistStalker instance;
-
+	*/
     // Says where the client and server 'proxy' code is loaded.
     @SidedProxy(clientSide="maknahr.mistStalker.proxy.ClientOnlyProxy", serverSide="maknahr.mistStalker.proxy.DedicatedServerProxy")
     public static CommonProxy proxy;
